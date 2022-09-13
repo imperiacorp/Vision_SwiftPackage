f9de1d1a3aea8a173963ab1ecb58e6746b8d6dfc
Dmitriy Gonchar <imperiacorp198801@gmail.com>
2022-09-13 09:02:54 +0300

    Initial Commit

---
+++ Sources/Vision.xcframework/ios-arm64/Vision.framework/Headers/Vision.h
@@ -0,0 +1,18 @@
+//
+//  Vision.h
+//  Vision
+//
+//  Created by Dmitriy Gonchar on 08.09.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Vision.
+FOUNDATION_EXPORT double VisionVersionNumber;
+
+//! Project version string for Vision.
+FOUNDATION_EXPORT const unsigned char VisionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Vision/PublicHeader.h>
+
+
